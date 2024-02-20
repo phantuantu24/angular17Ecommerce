25b81ef5be1a113e656a528178a73862cae12709
Phan Tuan Tu <phantuantu@secure.janeto.com>
2024-02-20 11:19:48 +0700

    init interface for all model

---
+++ src/app/core/Model/object-model.ts
@@ -0,0 +1,44 @@
+export interface User {
+  name: string;
+  password: string;
+  uploadPhoto: string;
+  role: string
+  mobNumber: string;
+  address: Address;
+  gender: string;
+  language: string[];
+  email: string;
+  dob: string;
+  agreetc: boolean;
+  age: number;
+  aboutYou: string;
+}
+
+export interface Address {
+  id: number;
+  addLine1: string;
+  addLine2: string;
+  city: string;
+  state: string;
+  zipCode: number
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  uploadPhoto: string;
+  uploadDesc: string;
+  mrp: number;
+  dp: number;
+  status: boolean
+}
+
+export interface Order {
+  id: number;
+  userId: number;
+  sellerId: number;
+  product: Product;
+  deliveryAddress: Address;
+  contact: number;
+  dateTime: string
+}
