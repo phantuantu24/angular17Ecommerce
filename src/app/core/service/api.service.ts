632a9c94c3d301863743214263eec2b2d1feecd6
Phan Tuan Tu <phantuantu@secure.janeto.com>
2024-02-20 11:04:38 +0700

    init api core service and loginSugnupService

--- src/app/core/service/api.service.ts
+++ src/app/core/service/api.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  httpOption = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    })
+  }
+
+  constructor(private http: HttpClient) { }
+
+  private formatError(error: any): Observable<never> {
+    return throwError(() => error.error)
+  }
+
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http.get(path, { params }).pipe(catchError(this.formatError))
+  }
+
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.http.put(path, JSON.stringify(body)).pipe(catchError(this.formatError))
+  }
+
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http.post(path, JSON.stringify(body)).pipe(catchError(this.formatError))
+  }
+
+  delete(path: string): Observable<any> {
+    return this.http.delete(path).pipe(catchError(this.formatError))
+  }
 }
--- src/app/shared/services/login-signup.service.ts
+++ src/app/shared/services/login-signup.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/service/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginSignupService {
 
-  constructor() { }
+  baseUrl: string = 'http://localhost:3000';
+
+  constructor(private http: HttpClient, private apiService: ApiService) { }
+
+  authLogin(userName: string, password: string) {
+    const url = `${this.baseUrl}/user?email=${userName}&password=${password}`;
+    return this.apiService.get(url);
+  }
+
+  userRegister(userDto: any) {
+    return this.apiService.post(`${this.baseUrl}/user`, userDto);
+  }
+
+  adminLogin(userName: string, password: string) {
+    const url = `${this.baseUrl}/user?email=${userName}&password=${password}&role=admin`;
+    return this.apiService.get(url);
+  }
 }
